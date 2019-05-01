dc29ad42790e32abc7adc23aad5c41417f9607ef
kowingyin <ken402402@gmail.com>
2019-05-02 03:57:33 +0800

    fe

--- frontend/movie-info-web/src/app/app.module.ts
+++ frontend/movie-info-web/src/app/app.module.ts
@@ -2,21 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
-import { HttpClientModule } from '@angular/common/http';
+import { FilmDetailsComponent } from './film-details/film-details.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    AboutComponent,
-    ContactComponent,
-    HomeComponent
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    FilmDetailsComponent
   ],
   imports: [
     BrowserModule,
--- frontend/movie-info-web/src/app/register/register.component.spec.ts
+++ frontend/movie-info-web/src/app/register/register.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ContactComponent } from './contact.component';
+import { RegisterComponent } from './register.component';
 
-describe('ContactComponent', () => {
-  let component: ContactComponent;
-  let fixture: ComponentFixture<ContactComponent>;
+describe('RegisterComponent', () => {
+  let component: RegisterComponent;
+  let fixture: ComponentFixture<RegisterComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ContactComponent ]
+      declarations: [ RegisterComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ContactComponent);
+    fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
