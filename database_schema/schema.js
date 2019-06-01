2e773cbd315d86991ba992bff8b8152063c63a0e
kowingyin <ken402402@gmail.com>
2019-06-02 05:16:08 +0800

    no message

--- database_schema/schema.js
+++ database_schema/schema.js
@@ -2,49 +2,49 @@
 
 // import the mongoose package
 const mongoose = require('mongoose')
-mongoose.connect(`mongodb://127.0.0.1:27017/cinema`, { useNewUrlParser: true })
+mongoose.connect(`mongodb://heroku_n33prb6t:Kw1235420.@ds231307.mlab.com:31307/heroku_n33prb6t`, { useNewUrlParser: true })
 mongoose.Promise = global.Promise
 const Schema = mongoose.Schema
 
 // create a user schema
 const userSchema = new Schema({
-	name: String,
-	username: String,
-	password: String,
-	email: String
+    name: String,
+    username: String,
+    password: String,
+    email: String
 })
 
 // create a film schema
 const filmSchema = new Schema({
-	account: String,
-	Title: String,
-	Year: String,
-	Runtime: String,
-	Director: String,
-	Plot: String,
-	imdbID: String,
+    account: String,
+    Title: String,
+    Year: String,
+    Runtime: String,
+    Director: String,
+    Plot: String,
+    imdbID: String,
 })
 
 const commentSchema = new Schema({
-	account: String,
-	Title: String,
-	Year: String,
-	Director: String,
-	imdbID: String,
-	comment: String,
-	time: String
+    account: String,
+    Title: String,
+    Year: String,
+    Director: String,
+    imdbID: String,
+    comment: String,
+    time: String
 })
 
 const ratingSchema = new Schema({
-	account: String,
-	Title: String,
-	Director: String,
-	imdbID: String,
-	rating: String,
+    account: String,
+    Title: String,
+    Director: String,
+    imdbID: String,
+    rating: String,
 })
 
 // create a model using the schema
 exports.Film = mongoose.model('Film', filmSchema)
 exports.User = mongoose.model('User', userSchema)
 exports.Comment = mongoose.model('Comment', commentSchema)
-exports.Rating = mongoose.model('Rating',ratingSchema)
+exports.Rating = mongoose.model('Rating', ratingSchema)
